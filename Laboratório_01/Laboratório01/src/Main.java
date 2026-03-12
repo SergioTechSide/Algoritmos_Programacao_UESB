3592d97b914090de564fdcfdfaf820682bbcc977
SergioTechSide <aviacao.contato@gmail.com>
2026-03-12 01:48:16 -0300

    Add files via upload
    Backup Inicial - Laboratórios 01 e 02 (IAP) Introdução a Algarítimos e Programação
    Descrição: Subindo a estrutura completa dos projetos Java realizados para a disciplina de Introdução a Algoritmos e Programação. Inclui o exercício de identificação e os exercícios de operadores (Soma, Poesia e Temperatura).  ... Descrição: > - Laboratório 01: Configuração de ambiente e classe Atividade01.
    
    Laboratório 02: Exercícios de entrada/saída e cálculos matemáticos.
    
    Configurações de codificação UTF-8 e estrutura de pastas do NetBeans incluídas.

---
+++ Laboratório_01/Laboratório01/src/Main.java
@@ -0,0 +1,18 @@
+
+/* * Disciplina: Introdução a Algoritmos e Programação
+* Atividade: Laboratório 01 - UESB
+ * Aluno: Sérgio Barros
+ */
+
+public class Main { 
+
+    public static void main(String[] args) {
+        
+        // Este comando de saída exibe o meu nome no console e pula uma linha
+        System.out.println("Meu nome é Sérgio");
+
+        // Este comando exibe a mensagem de identificação da disciplina
+        System.out.println("Esta é a primeira atividade da disciplina Introdução a Algoritmos e Programação");
+        
+    }
+}
